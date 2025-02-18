bfc1a3a547261211fbc9215fa3f172dbf3a43725
sulav singhal <sulavsinghal01@gmail.com>
2025-02-18 18:59:07 +0530

    Dynamic data in API signup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const connectdb = require("./config/database");
 const app = express();
+const User = require('./models/user');
+
+//creating API for adding data in database 
+app.post("/signup",async (req,res)=>{
+
+    // creating instance of the user model
+    const user = new User({
+        firstName: "Sulav",
+        lastName: "Singhal",
+        emailId: "sulavsinghal01@gmail.com",
+        password: "123",
+        age: 21,
+        gender: "Male"
+    });
+    await user.save();
+    res.send("User added sucessfully") // to save the data in database
+});
+
+
+
 
 connectdb()
 .then(()=>{
---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+// creating schemas using mongoose
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    }
+});
+
+// mongoose model creation
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
