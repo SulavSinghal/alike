c9944cb9c46ab4703313ae1fd01ce295f9fd380e
SulavSinghal <sulavsinghal01@gmail.com>
2025-03-06 23:25:21 +0530

    API user/connections/

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -10,6 +10,7 @@ const ConnectionRequestSchema = new mongoose.Schema({
     },
     toUserId: {
         type: mongoose.Schema.Types.ObjectId,
+        ref:"User", //
         required: true,
     },
     status: {
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender skills about";
 //Get all the pending connection Request for the loggedIn user
 userRouter.get("/user/requests/received",userAuth, async (req,res) =>{
 
@@ -11,7 +12,7 @@ userRouter.get("/user/requests/received",userAuth, async (req,res) =>{
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested",
-        }).populate("fromUserId","firstName lastName photoUrl age gender skills about");
+        }).populate("fromUserId",USER_SAFE_DATA);
 
         res.json({
             message: "Data fetched successfully",
@@ -22,4 +23,35 @@ userRouter.get("/user/requests/received",userAuth, async (req,res) =>{
         res.statusCode(400).send("Error: " + err.message);
     }
 });
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+
+    try{
+        const loggedInUser = req.user;
+        
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                    
+                    {toUserId: loggedInUser._id, status: "acccepted"},
+                    {fromUserId: loggedInUser._id, status: "accepted"},
+            ],
+        }).populate("fromUserId",USER_SAFE_DATA)
+           .populate("toUserId",USER_SAFE_DATA);
+
+        const data = connectionRequests.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+               return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+
+    }catch(err)
+    {
+        res.status(400).send({ message: err.message});
+    }
+});
+
+
+
 module.exports = userRouter;
